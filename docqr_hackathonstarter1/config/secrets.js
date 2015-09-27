6796dae597a57a5877558545d31808009b74858c
Jonatan Schumacher <jonatan@jschumacher.com>
2015-09-27 00:15:49 -0400

    init

---
+++ docqr_hackathonstarter1/config/secrets.js
@@ -0,0 +1,47 @@
+/**
+ * IMPORTANT  IMPORTANT  IMPORTANT  IMPORTANT  IMPORTANT  IMPORTANT  IMPORTANT
+ *
+ * You should never commit this file to a public repository on GitHub!
+ * All public code on GitHub can be searched, that means anyone can see your
+ * uploaded secrets.js file.
+ *
+ * I did it for your convenience using "throw away" API keys and passwords so
+ * that all features could work out of the box.
+ *
+ * Use config vars (environment variables) below for production API keys
+ * and passwords. Each PaaS (e.g. Heroku, Nodejitsu, OpenShift, Azure) has a way
+ * for you to set it up from the dashboard.
+ *
+ * Another added benefit of this approach is that you can use two different
+ * sets of keys for local development and production mode without making any
+ * changes to the code.
+
+ * IMPORTANT  IMPORTANT  IMPORTANT  IMPORTANT  IMPORTANT  IMPORTANT  IMPORTANT
+ */
+
+module.exports = {
+
+  db: process.env.MONGODB || process.env.MONGOLAB_URI || 'mongodb://localhost:27017/test',
+
+  sessionSecret: process.env.SESSION_SECRET || 'Your Session Secret goes here',
+
+  mailgun: {
+    user: process.env.MAILGUN_USER || 'postmaster@sandbox697fcddc09814c6b83718b9fd5d4e5dc.mailgun.org',
+    password: process.env.MAILGUN_PASSWORD || '29eldds1uri6'
+  },
+  
+  mandrill: {
+    user: process.env.MANDRILL_USER || 'hackathonstarterdemo',
+    password: process.env.MANDRILL_PASSWORD || 'E1K950_ydLR4mHw12a0ldA'
+  },
+
+  sendgrid: {
+    user: process.env.SENDGRID_USER || 'hslogin',
+    password: process.env.SENDGRID_PASSWORD || 'hspassword00'
+  },
+
+ 
+  clockwork: {
+    apiKey: process.env.CLOCKWORK_KEY || '9ffb267f88df55762f74ba2f517a66dc8bedac5a'
+  },
+};
